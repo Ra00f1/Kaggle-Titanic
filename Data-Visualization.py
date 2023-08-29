9930f25ca2da16eee6f15ef7e5e481cd6e4afdfc
Raoof Agh <32569954+Ra00f1@users.noreply.github.com>
2023-08-29 13:18:46 +0300

    Data Visualization completed

--- Data-Visualization.py
+++ Data-Visualization.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def Read_Data(filename):
     filename = "Data/" + filename + ".csv"
@@ -9,29 +10,22 @@ def Read_Data(filename):
 if __name__ == "__main__":
     train_data = Read_Data("train")
 
-    #plt_sex = plt.bar(train_data['Survived'], train_data['Sex'])
-    #plt_sex.title("Sex vs Survived")
-    #plt_sex.xlable('Sex')
-    #plt_sex.ylable('Survived')
+    print(train_data["Survived"].value_counts())
+    #print(train_data["SibSp"].value_counts())
+    #print(train_data["Parch"].value_counts())
 
-    #plt_age = plt.scatter(train_data['Age'], train_data['Survived'])
-    #plt_age.title("Age vs Survived")
-    #plt_age.xlable('Age')
-    #plt_age.ylable('Survived')
-#
-    plt_pclass = plt.bar(train_data['Pclass'], train_data['Survived'])
-    #plt_pclass.title("Pclass vs Survived")
-    #plt_pclass.xlable('Pclass')
-    #plt_pclass.ylable('Survived')
-#
-    #plt_sibps = plt.scatter(train_data['SibSp'], train_data['Survived'])
-    #plt_sibps.title("SibSp vs Survived")
-    #plt_sibps.xlable('SibSp')
-    #plt_sibps.ylable('Survived')
-#
-    #plt_parch = plt.scatter(train_data['Parch'], train_data['Survived'])
-    #plt_parch.title("Parch vs Survived")
-    #plt_parch.xlable('Parch')
-    #plt_parch.ylable('Survived')
+    f = plt.figure(1)
+    sns.barplot(x='Survived', y='Age', data=train_data, hue='Sex')
+    f.show()
 
-    plt.show()
+    g = plt.figure(2)
+    sns.barplot(x='Survived', y='Pclass', data=train_data, hue='Sex')
+    g.show()
+
+    h = plt.figure(3)
+    sns.barplot(x='Survived', y='SibSp', data=train_data, hue='Sex')
+    h.show()
+
+    y = plt.figure(3)
+    sns.barplot(x='Survived', y='Parch', data=train_data, hue='Sex')
+    y.show()
