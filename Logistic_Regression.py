26155ef7ec969c2308a59f9b36d646a678ecb042
Raoof Agh <32569954+Ra00f1@users.noreply.github.com>
2023-09-05 16:03:09 +0300

    Some small touches

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -1,31 +1,34 @@
-from sklearn.model_selection import train_test_split
 import Data
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
 import pandas as pd
 
-if __name__ == "__main__":
-    train_data = Data.Read_Data("train")
-    test_data = Data.Read_Data("test")
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
-    # Delete unwanted columns
-    train_data = train_data.drop(["PassengerId", "Name", "Ticket", "Cabin", "Embarked", "Fare"], axis=1)
+def kaggle_test(log_reg):
+    print("Kaggle Test Started")
+    test_data = Data.Read_Data("test")
+    test_data_modified = test_data.drop(["PassengerId", "Name", "Ticket", "Cabin", "Embarked", "Fare"], axis=1)
 
     # Label Encoding
-    # male = 0, female = 1
-    train_data.replace(["male", "female"], [0, 1], inplace=True)
-
-    X_train = train_data.drop(["Survived"], axis=1)
-    y_train = train_data["Survived"]
-
-    # Logistic Regression
-    Scaler = StandardScaler()
+    test_data_modified.replace(["male", "female"], [0, 1], inplace=True)
 
     # Fill missing Age values with mean
-    # Test with both this and deleted row one
-    X_train["Age"].fillna(X_train["Age"].mean(), inplace=True)
+    test_data_modified["Age"].fillna(test_data_modified["Age"].mean(), inplace=True)
 
-    # Note: Fit X_train only and transform X_test only
+    # Feature Scaling (transform only)
+    X_test = Scaler.transform(test_data_modified)
+
+    # Predicting the test data for Kaggle
+    y_pred = log_reg.predict(X_test)
+
+    # Saving the predictions and passenger id to csv file
+    df = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': y_pred})
+    df.to_csv('Data/Logistic_Regression.csv', index=False)
+    print("Kaggle Test Completed")
+
+def logistik_Regression(X_train, y_train):
+    print("Logistic Regression Started")
     X_train = Scaler.fit_transform(X_train)
 
     X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
@@ -38,23 +41,34 @@ if __name__ == "__main__":
 
     # Testing the model
     print("Testing Accuracy: ", log_reg.score(X_test, y_test))
+    return log_reg
 
-    # Predicting the test data for Kaggle
-    test_data_modified = test_data.drop(["PassengerId", "Name", "Ticket", "Cabin", "Embarked", "Fare"], axis=1)
+if __name__ == "__main__":
+    train_data = Data.Read_Data("train")
+
+    # Delete unwanted columns
+    train_data = train_data.drop(["PassengerId", "Name", "Ticket", "Cabin", "Embarked", "Fare"], axis=1)
 
     # Label Encoding
-    test_data_modified.replace(["male", "female"], [0, 1], inplace=True)
+    # male = 0, female = 1
+    train_data.replace(["male", "female"], [0, 1], inplace=True)
+
+    # Splitting the data
+    X_train = train_data.drop(["Survived"], axis=1)
+    y_train = train_data["Survived"]
+
+    # Feature Scaling
+    Scaler = StandardScaler()
+
     # Fill missing Age values with mean
-    test_data_modified["Age"].fillna(test_data_modified["Age"].mean(), inplace=True)
+    X_train["Age"].fillna(X_train["Age"].mean(), inplace=True)
 
-    X_test = Scaler.transform(test_data_modified)
+    # Note: Fit X_train only and transform X_test only
 
+    log_reg = logistik_Regression(X_train, y_train)
     # Predicting the test data for Kaggle
-    y_pred = log_reg.predict(X_test)
+    kaggle_test(log_reg)
 
-    # Saving the predictions and passenger id to csv file
-    df = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': y_pred})
-    df.to_csv('Data/Logistic_Regression.csv', index=False)
 
 
 
